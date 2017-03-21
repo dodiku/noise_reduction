cd34ff6e80ea544c8743582ddcb33aa517cd07e4
dodiku <dda290@nyu.edu>
2017-03-21 04:36:00 +0200

    done. good night zzzz..

--- noise.py
+++ noise.py
@@ -67,7 +67,7 @@ receives an audio matrix,
 returns the same matrix after audio manipulation
 ------------------------------------'''
 def enhance(y):
-    apply_audio_effects = AudioEffectsChain().lowshelf(gain=12.0, frequency=260, slope=0.1).reverb(reverberance=25, hf_damping=5, room_scale=5, stereo_depth=50, pre_delay=20, wet_gain=0, wet_only=False)#.normalize()
+    apply_audio_effects = AudioEffectsChain().lowshelf(gain=10.0, frequency=260, slope=0.1).reverb(reverberance=25, hf_damping=5, room_scale=5, stereo_depth=50, pre_delay=20, wet_gain=0, wet_only=False)#.normalize()
     y_enhanced = apply_audio_effects(y)
 
     return y_enhanced
