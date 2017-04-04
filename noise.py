74ec07476e5556f0da3e50b6e6b8ddf1beac12e1
dodiku <dda290@nyu.edu>
2017-04-04 17:36:10 -0400

    improvements + new techniques. need to update the landing page

--- noise.py
+++ noise.py
@@ -1,12 +1,20 @@
 import librosa
 from pysndfx import AudioEffectsChain
 import numpy as np
+import math
+# from python_speech_features import mfcc
+# from python_speech_features import logfbank
+import python_speech_features
+
+# http://python-speech-features.readthedocs.io/en/latest/
+# https://github.com/jameslyons/python_speech_features
+# http://practicalcryptography.com/miscellaneous/machine-learning/guide-mel-frequency-cepstral-coefficients-mfccs/#deltas-and-delta-deltas
 
 
 '''------------------------------------
 FILE READER:
-receives filename,
-returns audio time series (y) and sampling rate of y (sr)
+    receives filename,
+    returns audio time series (y) and sampling rate of y (sr)
 ------------------------------------'''
 def read_file(file_name):
     sample_file = file_name
@@ -20,51 +28,151 @@ def read_file(file_name):
 
 '''------------------------------------
 NOISE REDUCTION USING POWER:
-receives an audio matrix,
-returns the matrix after gain reduction on noise
+    receives an audio matrix,
+    returns the matrix after gain reduction on noise
 ------------------------------------'''
-def reduce_noise_power(y):
+def reduce_noise_power(y, sr):
 
-    # using power
-    # S = np.abs(librosa.stft(y))
-    # S_power = librosa.power_to_db(S**2)
-    # print (S_power.shape)
-    # print (S_power)
-    # print (S_power.max())
-    # print (S_power/S_power.max())
-
-
-    # using the centroid
     cent = librosa.feature.spectral_centroid(y=y, sr=sr)
-    # print (cent.shape)
-    # print (cent)
-    # print (np.mean(cent))
-    # print (np.median(cent))
 
     threshold_h = round(np.median(cent))*1.5
     threshold_l = round(np.median(cent))*0.2
+
     less_noise = AudioEffectsChain().lowshelf(gain=-12.0, frequency=threshold_l, slope=0.5).highshelf(gain=-12.0, frequency=threshold_h, slope=0.5)
     y_clean = less_noise(y)
-    # print (y_clean)
 
     return y_clean
 
 
 '''------------------------------------
+NOISE REDUCTION USING CENTROID ANALYSIS:
+    receives an audio matrix,
+    returns the matrix after gain reduction on noise
+------------------------------------'''
+
+def reduce_noise_centroid_s(y, sr):
+
+    cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+
+    threshold_h = np.max(cent)
+    threshold_l = np.min(cent)
+
+    less_noise = AudioEffectsChain().lowshelf(gain=-12.0, frequency=threshold_l, slope=0.5).highshelf(gain=-12.0, frequency=threshold_h, slope=0.5).limiter(gain=6.0)
+
+    y_cleaned = less_noise(y)
+
+    return y_cleaned
+
+def reduce_noise_centroid_mb(y, sr):
+
+    cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+
+    threshold_h = np.max(cent)
+    threshold_l = np.min(cent)
+
+    less_noise = AudioEffectsChain().lowshelf(gain=-30.0, frequency=threshold_l, slope=0.5).highshelf(gain=-30.0, frequency=threshold_h, slope=0.5).limiter(gain=10.0)
+    # less_noise = AudioEffectsChain().lowpass(frequency=threshold_h).highpass(frequency=threshold_l)
+    y_cleaned = less_noise(y)
+
+
+    cent_cleaned = librosa.feature.spectral_centroid(y=y_cleaned, sr=sr)
+    columns, rows = cent_cleaned.shape
+    boost_h = math.floor(rows/3*2)
+    boost_l = math.floor(rows/6)
+    boost = math.floor(rows/3)
+
+    # boost_bass = AudioEffectsChain().lowshelf(gain=20.0, frequency=boost, slope=0.8)
+    boost_bass = AudioEffectsChain().lowshelf(gain=16.0, frequency=boost_h, slope=0.5)#.lowshelf(gain=-20.0, frequency=boost_l, slope=0.8)
+    y_clean_boosted = boost_bass(y_cleaned)
+
+    return y_clean_boosted
+
+
+'''------------------------------------
+NOISE REDUCTION USING MFCC:
+    receives an audio matrix,
+    returns the matrix after gain reduction on noise
+------------------------------------'''
+def reduce_noise_mfcc_down(y, sr):
+
+    hop_length = 512
+
+    ## librosa
+    # mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=13)
+    # librosa.mel_to_hz(mfcc)
+
+    ## mfcc
+    mfcc = python_speech_features.base.mfcc(y)
+    mfcc = python_speech_features.base.logfbank(y)
+    mfcc = python_speech_features.base.lifter(mfcc)
+
+    sum_of_squares = []
+    index = -1
+    for r in mfcc:
+        sum_of_squares.append(0)
+        index = index + 1
+        for n in r:
+            sum_of_squares[index] = sum_of_squares[index] + n**2
+
+    strongest_frame = sum_of_squares.index(max(sum_of_squares))
+    hz = python_speech_features.base.mel2hz(mfcc[strongest_frame])
+
+    max_hz = max(hz)
+    min_hz = min(hz)
+
+    speech_booster = AudioEffectsChain().highshelf(frequency=min_hz*(-1)*1.2, gain=-12.0, slope=0.6).limiter(gain=8.0)
+    y_speach_boosted = speech_booster(y)
+
+    return (y_speach_boosted)
+
+def reduce_noise_mfcc_up(y, sr):
+
+    hop_length = 512
+
+    ## librosa
+    # mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=13)
+    # librosa.mel_to_hz(mfcc)
+
+    ## mfcc
+    mfcc = python_speech_features.base.mfcc(y)
+    mfcc = python_speech_features.base.logfbank(y)
+    mfcc = python_speech_features.base.lifter(mfcc)
+
+    sum_of_squares = []
+    index = -1
+    for r in mfcc:
+        sum_of_squares.append(0)
+        index = index + 1
+        for n in r:
+            sum_of_squares[index] = sum_of_squares[index] + n**2
+
+    strongest_frame = sum_of_squares.index(max(sum_of_squares))
+    hz = python_speech_features.base.mel2hz(mfcc[strongest_frame])
+
+    max_hz = max(hz)
+    min_hz = min(hz)
+
+    speech_booster = AudioEffectsChain().lowshelf(frequency=min_hz*(-1), gain=12.0, slope=0.5)#.highshelf(frequency=min_hz*(-1)*1.2, gain=-12.0, slope=0.5)#.limiter(gain=8.0)
+    y_speach_boosted = speech_booster(y)
+
+    return (y_speach_boosted)
+
+'''------------------------------------
 SILENCE TRIMMER:
-receives an audio matrix,
-returns an audio matrix with almost no silence
+    receives an audio matrix,
+    returns an audio matrix with less silence and the amout of time that was trimmed
 ------------------------------------'''
 def trim_silence(y):
-    y_trimmed, index = librosa.effects.trim(y, top_db=12, frame_length=2)
+    y_trimmed, index = librosa.effects.trim(y, top_db=20, frame_length=2, hop_length=500)
+    trimmed_length = librosa.get_duration(y) - librosa.get_duration(y_trimmed)
 
-    return y_trimmed
+    return y_trimmed, trimmed_length
 
 
 '''------------------------------------
 AUDIO ENHANCER:
-receives an audio matrix,
-returns the same matrix after audio manipulation
+    receives an audio matrix,
+    returns the same matrix after audio manipulation
 ------------------------------------'''
 def enhance(y):
     apply_audio_effects = AudioEffectsChain().lowshelf(gain=10.0, frequency=260, slope=0.1).reverb(reverberance=25, hf_damping=5, room_scale=5, stereo_depth=50, pre_delay=20, wet_gain=0, wet_only=False)#.normalize()
@@ -74,30 +182,30 @@ def enhance(y):
 
 '''------------------------------------
 OUTPUT GENERATOR:
-receives a destination path, file name, audio matrix, and sample rate,
-generates a wav file based on input
+    receives a destination path, file name, audio matrix, and sample rate,
+    generates a wav file based on input
 ------------------------------------'''
-def output_file(destination ,filename, y, sr):
-    destination = destination + filename[:-4] + '.wav'
+def output_file(destination ,filename, y, sr, ext=""):
+    destination = destination + filename[:-4] + ext + '.wav'
     librosa.output.write_wav(destination, y, sr)
 
 
 '''------------------------------------
-LOGIC
+LOGIC:
+    [1] load file
+    [2] reduce noise
+    [3] trim silence
+    [4] output file
 
-files:
-01_counting.m4a
-02_wind_and_cars.m4a
-03_truck.m4a
-04_voices.m4a
-05_ambeint.m4a
-06_office.m4a
+sample files:
+    01_counting.m4a
+    02_wind_and_cars.m4a
+    03_truck.m4a
+    04_voices.m4a
+    05_ambeint.m4a
+    06_office.m4a
 ------------------------------------'''
 samples = ['01_counting.m4a','02_wind_and_cars.m4a','03_truck.m4a','04_voices.m4a','05_ambeint.m4a','06_office.m4a']
-
-# destination paths
-trimmed_destination = 'samples_trimmed/'
-noise_destination = 'samples_noise_reduced/'
 
 for s in samples:
     # reading a file
@@ -105,19 +213,35 @@ for s in samples:
     y, sr = read_file(filename)
 
     # reducing noise using db power
-    y_reduced_power = reduce_noise_power(y)
-
-    # # generating output file [1]
-    output_file('01_samples_noise_reduced/' ,filename, y_reduced_power, sr)
+    y_reduced_power = reduce_noise_power(y, sr)
+    y_reduced_centroid_s = reduce_noise_centroid_s(y, sr)
+    y_reduced_centroid_mb = reduce_noise_centroid_mb(y, sr)
+    y_reduced_mfcc_up = reduce_noise_mfcc_up(y, sr)
+    y_reduced_mfcc_down = reduce_noise_mfcc_down(y, sr)
 
     # trimming silences
-    y_trimmed_power = trim_silence(y_reduced_power)
+    y_reduced_power, time_trimmed = trim_silence(y_reduced_power)
+    # print (time_trimmed)
 
-    # generating output file [2]
-    output_file('02_samples_trimmed/' ,filename, y_trimmed_power, sr)
+    y_reduced_centroid_s, time_trimmed = trim_silence(y_reduced_centroid_s)
+    # print (time_trimmed)
 
-    # enhancing sound style
-    y_enhanced = enhance(y_trimmed_power)
+    y_reduced_power, time_trimmed = trim_silence(y_reduced_power)
+    # print (time_trimmed)
 
-    # generating output file [3]
-    output_file('03_samples_enhanced/' ,filename, y_enhanced, sr)
+    y_reduced_centroid_mb, time_trimmed = trim_silence(y_reduced_centroid_mb)
+    # print (time_trimmed)
+
+    y_reduced_mfcc_up, time_trimmed = trim_silence(y_reduced_mfcc_up)
+    # print (time_trimmed)
+
+    y_reduced_mfcc_down, time_trimmed = trim_silence(y_reduced_mfcc_down)
+    # print (time_trimmed)
+
+    # generating output file [1]
+    output_file('01_samples_trimmed_noise_reduced/' ,filename, y_reduced_power, sr, '_pwr')
+    output_file('01_samples_trimmed_noise_reduced/' ,filename, y_reduced_centroid_s, sr, '_ctr_s')
+    output_file('01_samples_trimmed_noise_reduced/' ,filename, y_reduced_centroid_mb, sr, '_ctr_mb')
+    output_file('01_samples_trimmed_noise_reduced/' ,filename, y_reduced_mfcc_up, sr, '_mfcc_up')
+    output_file('01_samples_trimmed_noise_reduced/' ,filename, y_reduced_mfcc_down, sr, '_mfcc_down')
+    output_file('01_samples_trimmed_noise_reduced/' ,filename, y, sr, '_org')
